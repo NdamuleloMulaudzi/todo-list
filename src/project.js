6354b6c3156ce54791650c7cb5d1f8e7ecdc73ad
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-09-14 10:09:25 +0200

    add logic to store tasks inside a project

--- src/project.js
+++ src/project.js
@@ -1,16 +1,18 @@
-import Task from "./task";
-
 export default class Project {
   constructor(name) {
     this.name = name;
     this.tasks = [];
   }
+
   addTask(task) {
     this.tasks.push(task);
   }
-  diplayTask() {
-    return this.tasks.map((task) => {
-      return `Task: ${task.title}, Description: ${task.description}, Due Date: ${task.dueDate}, Priority: ${task.priority}`;
-    });
+
+  removeTask(taskTitle) {
+    this.tasks = this.tasks.filter((task) => task.title !== taskTitle);
+  }
+
+  getTasks() {
+    return this.tasks;
   }
 }
